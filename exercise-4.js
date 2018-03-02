958debacdbad43a996fa156a681b19a5a8e072bd
didi <pesandidi@gmail.com>
2018-03-03 03:23:29 +0700

    exercise-4

---
+++ exercise-4.js
@@ -0,0 +1,30 @@
+var tanggal = 24;
+var bulan = 5;
+var tahun = 1991;
+
+switch(bulan) {
+    case 1:  tanggal + ' Januari ' + tahun;
+    break; 
+    case 2:  tanggal + ' Februari ' + tahun; 
+    break; 
+    case 3:  tanggal + ' Maret ' + tahun; 
+    break; 
+    case 4:  tanggal + ' April ' + tahun; 
+    break; 
+    case 5:  tanggal + ' Mei ' + tahun; 
+    break; 
+    case 6:  tanggal + ' Juni ' + tahun;
+    break;   
+    case 7: tanggal + ' Juli ' + tahun; 
+    break; 
+    case 8: tanggal + ' Agustus ' + tahun; 
+    break; 
+    case 9: tanggal + ' September ' + tahun; 
+    break; 
+    case 10:tanggal + ' Oktober ' + tahun; 
+    break; 
+    case 11:tanggal + ' November ' + tahun; 
+    break;
+    case 12:tanggal + ' Desember ' + tahun;
+     break; 
+             }
