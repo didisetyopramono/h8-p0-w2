94f241b7017fe693ebab8e9f6696d8728b46ae1e
didisetyopramono <36652543+didisetyopramono@users.noreply.github.com>
2018-03-05 19:59:59 +0700

    Update exercise-3.js

--- exercise-3.js
+++ exercise-3.js
@@ -1,27 +1,21 @@
- var nama="";
- var peran="";
+var nama = '';
+var peran = '';
 
-    if((nama==="")&&(peran==="")){
-     console.log("Nama harus diisi!");
-     } 
-     else if((nama=="Mikael")&&(peran==="")){
-     console.log("Halo Mikael, Pilih peranmu untuk memulai game!");
- 
-     }
-     else if((nama==="Nina")&&(peran==="Ksatria")){
-     console.log("Selamat datang di Dunia Proxytia, Nina");
-     console.log("Halo Ksatria Nina, kamu dapat menyerang dengan senjatamu!");
-  
-     }
+        if (nama === ''){
+        console.log('nama harus diisi!');
+        } 
+        else if (peran ==='Ksatria')
+        {
+        console.log('Selamat datang di Dunia Proxytia, '+nama+'. Halo Ksatria '+nama+', 
+                    Kamu dapat menyerang dengan senjatamu!');
+        }       
 
-     else if((nama==="Danu")&&(peran==="Tabib")){
-     console.log("Selamat datang di Dunia Proxytia, Danu");
-     console.log("Halo Tabib Danu, kamu akan membantu temanmu yang terluka.");
-     }
-
-     else if((nama==="Zero")&&(peran="Penyihir")){
-  
-     console.log( "Selamat datang di Dunia Proxytia, Zero");
-
-     console.log("Halo Penyihir Zero, ciptakan keajaiban yang membantu kemenanganmu ");
-     } 
+        else if (peran === 'Tabib')
+        {
+        console.log('Selamat datang di Dunia Proxytia, '+nama+'. Halo Tabib '+nama+',
+                    Kamu akan membantu temanmu yang terluka.');
+        } 
+        else if (peran === 'Penyihir'){
+        console.log('Selamat datang di Dunia Proxytia, '+nama+'. Halo Penyihir '+nama+',
+                    ciptakan keajaiban yang membantu kemenanganmu!');
+        }
