9b14c722731da524b5e7e0369684426d9241120f
didisetyopramono <36652543+didisetyopramono@users.noreply.github.com>
2018-03-06 10:32:14 +0700

    Update exercise4.js

--- exercise4.js
+++ exercise4.js
@@ -7,19 +7,19 @@ console.log(input);
 
  var bulan = 05;
  switch(bulan) {
-   case 1:    {console.log('Januari'); break;}
-   case 2:    {console.log('Februari'); break;}
-   case 3:    {console.log('Maret'); break;}
-   case 4:    {console.log('April'); break;}
-   case 5:    {console.log('Mei'); break;}
-   case 6:    {console.log('Juni'); break;}
-   case 7:    {console.log('Juli'); break;}
-   case 8:    {console.log('Agustus'); break;}
-   case 9:    {console.log('September'); break;}
-   case 10:    {console.log('Oktober'); break;}
-   case 11:    {console.log('November'); break;}
-   case 12:    {console.log('Desember'); break;}
-   default:   {console.log('ngaco'); }
+   case 1:    'Januari'; break;
+   case 2:    'Februari'; break;
+   case 3:    'Maret'; break;
+   case 4:    'April'; break;
+   case 5:    'Mei'; break;
+   case 6:    'Juni'; break;
+   case 7:    'Juli'; break;
+   case 8:    'Agustus'; break;
+   case 9:    'September'; break;
+   case 10:   'Oktober'; break;
+   case 11:   'November'; break;
+   case 12:   'Desember'; break;
+   default:   'HmmmmMMMmmmm!!!'; 
  }
 
 var format = input[3];
